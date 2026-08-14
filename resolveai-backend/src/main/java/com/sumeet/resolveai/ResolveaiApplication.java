7aee512080b4db563759c961ecf715df68258428
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2026-08-14 21:46:50 +0530

    initialised backend

---
+++ resolveai-backend/src/main/java/com/sumeet/resolveai/ResolveaiApplication.java
@@ -0,0 +1,13 @@
+package com.sumeet.resolveai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolveaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolveaiApplication.class, args);
+	}
+
+}
---
+++ resolveai-backend/src/test/java/com/sumeet/resolveai/ResolveaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumeet.resolveai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResolveaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
